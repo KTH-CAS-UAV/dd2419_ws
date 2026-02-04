350046334a347bc93ccc1aa03e32c8c0904b8a44
Clemens Faller <cfaller@kth.se>
2026-02-04 10:58:58 +0100

    added the driver package that features keyboard driving

--- src/driver/driver/driver.py
+++ src/driver/driver/driver.py
@@ -10,16 +10,32 @@ class Driver(Node):
         self.pub = self.create_publisher(DutyCycles, '/phidgets/motor/duty_cycles', 10)
 
     def send_msg_stop(self):
+        #self.get_logger().info(f'send_msg_stop function was entered')
         msg = DutyCycles()
         msg.duty_cycle_left = 0
         msg.duty_cycle_right = 0
         self.pub.publish(msg)
 
-    def on_press(key, self):
+    def send_msg_change_vel(self, left, right):
+        msg = DutyCycles()
+        msg.duty_cycle_left = left
+        msg.duty_cycle_right = right
+        self.pub.publish(msg)
+
+    def on_press(self, key):
         #print(f'Key {key.char} pressed')
+        #self.get_logger().info(f'send_msg_stop function was entered')
         try:
-            if key.char == 'm':
-                self.send_msg_stop()
+            if key.char == 'q':
+                self.send_msg_change_vel(0, 0)
+            elif key.char == 'w':
+                self.send_msg_change_vel(0.2, 0.2)
+            elif key.char == 'a':
+                self.send_msg_change_vel(-0.2, 0.2)
+            elif key.char == 'd':
+                self.send_msg_change_vel(0.2, -0.2)
+            elif key.char == 's':
+                self.send_msg_change_vel(-0.2, -0.2)
         except AttributeError:
             pass
             
@@ -27,16 +43,17 @@ class Driver(Node):
 def main():
     rclpy.init()
     node = Driver()
-
+    #rclpy.spin_once(node)
     listener = keyboard.Listener(
         on_press=node.on_press)#,
         #on_release=on_release)
     listener.start() #on a seperate thread (non-blocking)
+    rclpy.spin(node)
+    #rclpy.spin_once(node)
 
     # while(True):
     #      continue
 
-    rclpy.spin(node)
 
     rclpy.shutdown()
 
